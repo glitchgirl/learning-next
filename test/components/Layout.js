c4303bd5becca4096598f02fd4b76377fa81e9d8
Morgan <londonspleen@gmail.com>
2022-09-06 09:45:31 -0400

    init

---
+++ test/components/Layout.js
@@ -0,0 +1,11 @@
+import { Children } from "react"
+
+const Layout = () =>{
+    return (
+        <div>
+            <main>{Children}</main>
+        </div>
+    )
+}
+
+export default Layout
---
+++ test/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+   <Head>
+    <title>hewwo</title>
+    <meta>so cool tags</meta>
+   </Head>
+    </div>
+  )
+}
